bf6e613321025a1f1c4aa614a9b93c33a4468feb
rutikapujari <rpujari5000@gmail.com>
2026-05-23 15:50:00 +0530

    updated service

--- backend/src/modules/admin/routes/dashboardRoutes.js
+++ backend/src/modules/admin/routes/dashboardRoutes.js
@@ -1,5 +1,4 @@
 const express = require("express");
-
 const router = express.Router();
 
 const {
@@ -8,5 +7,9 @@ const {
 
 // GET Dashboard Data
 router.get("/", getDashboardStats);
+
+// API ROUTE
+router.get("/stats", getDashboardStats);
+
 
 module.exports = router;
