83f790d56f548e9db74e1428ca90c26bc9067fd5
Siddhi Naik <siddhisnaik04@gmail.com>
2026-05-26 15:28:31 +0530

    new

--- backend/backend/src/models/inventaryManagers/Warehouse.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const warehouseSchema = new mongoose.Schema({
-  name: String,
-  location: String,
-});
-
-module.exports = mongoose.model("Warehouse", warehouseSchema);
